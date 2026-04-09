65a9f4ed202e8f6dd0d81bf2ceeee116483d4b67
akhanal222 <ashirvad.khanal@gmail.com>
2026-04-09 10:42:21 -0400

    fix: correct link path

--- frontend/src/pages/MatchupsDashboard.jsx
+++ frontend/src/pages/MatchupsDashboard.jsx
@@ -11,7 +11,7 @@ import {
     Legend,
 } from "chart.js";
 import NavBar from "../components/Navbar.jsx";
-import PlayerVsPlayer from "./PlayerVsPlayer.jsx";
+import PlayerVsPlayer from "./Playervsplayer.jsx";
 import "../matchup.css";
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Tooltip, Legend);
