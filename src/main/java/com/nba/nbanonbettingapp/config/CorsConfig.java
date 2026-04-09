95ba60e84e814dbc3c3821c261c82915a7e6af52
akhanal222 <ashirvad.khanal@gmail.com>
2026-04-09 11:52:00 -0400

    fix: Path so Vercel will work

--- src/main/java/com/nba/nbanonbettingapp/config/CorsConfig.java
+++ src/main/java/com/nba/nbanonbettingapp/config/CorsConfig.java
@@ -14,7 +14,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://nba-non-betting-app.vercel.app",
+                                "https://nba-non-betting-app-git-main-ashirvads-projects-fc6d05b6.vercel.app",
+                                "https://nba-non-betting-app-git-feat-514c06-ashirvads-projects-fc6d05b6.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
