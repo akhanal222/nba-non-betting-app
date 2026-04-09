559d999e505d3df796c493797687151d4bdebafe
akhanal222 <ashirvad.khanal@gmail.com>
2026-04-09 10:53:46 -0400

    fix: correct link path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/Navbar.jsx";
 import PlayerCard from "./components/Playercard.jsx";
 
 const API = {
